d16cc2074c9476de5b56e866b837fe14a83b7b45
Anmol Kumar <110680449+anmol2517@users.noreply.github.com>
2024-12-31 18:03:35 +0530

    Update ConnectionFactory.java

--- src/com/inventory/Database/ConnectionFactory.java
+++ src/com/inventory/Database/ConnectionFactory.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.inventory.Database;
 
 import java.io.FileInputStream;
@@ -14,11 +10,6 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Properties;
-
-/**
- *
- * @author asjad
- */
 
 //Class to retrieve connection for database and login verfication.
 public class ConnectionFactory {
