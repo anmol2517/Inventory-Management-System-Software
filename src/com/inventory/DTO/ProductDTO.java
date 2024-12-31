5a7194a1b1d7756d4ebdf04de1d2b6e113e6a58e
Anmol Kumar <110680449+anmol2517@users.noreply.github.com>
2024-12-31 18:05:03 +0530

    Update ProductDTO.java

--- src/com/inventory/DTO/ProductDTO.java
+++ src/com/inventory/DTO/ProductDTO.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.inventory.DTO;
 
-/**
- *
- * @author asjad
- */
+package com.inventory.DTO;
 
 // Data Transfer Object (DTO) class for Products
 
