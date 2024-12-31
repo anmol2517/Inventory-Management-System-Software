0309c3e151f1e3e4a49493a51423773fd1dc942a
Anmol Kumar <110680449+anmol2517@users.noreply.github.com>
2024-12-31 18:06:02 +0530

    Update CustomerDAO.java

--- src/com/inventory/DAO/CustomerDAO.java
+++ src/com/inventory/DAO/CustomerDAO.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.inventory.DAO;
 
 import com.inventory.DTO.CustomerDTO;
@@ -14,12 +10,8 @@ import java.sql.*;
 import java.util.Locale;
 import java.util.Vector;
 
-/**
- *
- * @author asjad
- */
 
-// Data Access Object for Customers
+
 public class CustomerDAO {
     Connection conn = null;
     PreparedStatement prepStatement= null;
