1d41c8e5995b4cce35337dc8fcef41b69b4ceb8d
Anmol Kumar <110680449+anmol2517@users.noreply.github.com>
2024-12-31 17:58:27 +0530

    Update LoginPage.java

--- src/com/inventory/UI/LoginPage.java
+++ src/com/inventory/UI/LoginPage.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.inventory.UI;
 
 
@@ -17,10 +13,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.*;
 
-/**
- *
- * @author asjad
- */
+
 public class LoginPage extends javax.swing.JFrame {
 
     UserDTO userDTO;
