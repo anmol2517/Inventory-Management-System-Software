9942cd1ee5c681fb456200b58b258b01b605f480
Anmol Kumar <110680449+anmol2517@users.noreply.github.com>
2024-12-31 18:06:19 +0530

    Update ProductDAO.java

--- src/com/inventory/DAO/ProductDAO.java
+++ src/com/inventory/DAO/ProductDAO.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.inventory.DAO;
 
 import com.inventory.DTO.ProductDTO;
@@ -14,12 +10,6 @@ import java.sql.*;
 import java.util.Locale;
 import java.util.Vector;
 
-/**
- *
- * @author asjad
- */
-
-// Data Access Object for Products, Purchase, Stock and Sales
 public class ProductDAO {
 
     Connection conn = null;
