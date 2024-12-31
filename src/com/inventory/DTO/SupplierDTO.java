40c1b1fb466ba6b3adea8aec253b6604f6514536
Anmol Kumar <110680449+anmol2517@users.noreply.github.com>
2024-12-31 18:05:21 +0530

    Update SupplierDTO.java

--- src/com/inventory/DTO/SupplierDTO.java
+++ src/com/inventory/DTO/SupplierDTO.java
@@ -1,16 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.inventory.DTO;
-
-/**
- *
- * @author asjad
- */
-
-// Data Transfer Object (DTO) class for Suppliers
 
 public class SupplierDTO {
 
